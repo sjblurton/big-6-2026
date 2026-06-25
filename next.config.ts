c24bcd6b86fecc4c94caf7cc27c82c9a3bd5b283
Simon Blurton <sblurton@hotmail.co.uk>
2026-06-25 15:22:42 +0100

    Set up environment variables for Supabase and add client/server configuration

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+import "./src/lib/env/client";
+import "./src/lib/env/server";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
---
+++ src/lib/env/server.ts
@@ -0,0 +1,23 @@
+import { createEnv } from "@t3-oss/env-nextjs"; // or core package
+import * as z from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    SUPABASE_SECRET_KEY: z.string().min(1),
+    SUPABASE_DATABASE_URL: z.string().min(1),
+  },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY,
+    SUPABASE_DATABASE_URL: process.env.SUPABASE_DATABASE_URL,
+  },
+});
