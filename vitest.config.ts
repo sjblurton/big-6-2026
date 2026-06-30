5eee329ac88af7172cef341c11bc31ef72834836
Simon Blurton <sblurton@hotmail.co.uk>
2026-06-30 17:14:40 +0100

    chore: update vitest configuration to integrate Playwright and enable tsconfig paths resolution

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
+
 const dirname =
   typeof __dirname !== "undefined"
     ? __dirname
@@ -11,7 +12,10 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     projects: [
       {
@@ -33,6 +37,7 @@ export default defineConfig({
         test: {
           name: "storybook",
           browser: {
+            provider: playwright(),
             enabled: true,
             headless: true,
             instances: [
